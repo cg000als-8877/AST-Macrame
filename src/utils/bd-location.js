06237bc1c4bcd9537f9fe121f1e05628685f9686
cg000als-8877 <cg000als@gmail.com>
2026-08-11 11:36:58 +0600

    2

--- src/utils/bd-location.js
+++ src/utils/bd-location.js
@@ -4,16 +4,56 @@ import upazilasRaw from '../data/bd-upazilas.json';
 const districts = districtsRaw.districts;
 const upazilas = upazilasRaw.upazilas;
 
-const dhakaMetroThanas = [
-  "Adabor", "Airport", "Badda", "Banani", "Bangshal", "Bhashantek", "Cantonment", 
-  "Chawkbazar", "Dakshinkhan", "Dar-us-Salam", "Demra", "Dhanmondi", "Gendaria", 
-  "Gulshan", "Hatirjheel", "Hazaribagh", "Jatrabari", "Kadamtali", "Kafrul", 
-  "Kalabagan", "Kamrangirchar", "Khilgaon", "Khilkhet", "Kotwali", "Lalbagh", 
-  "Mirpur", "Mohammadpur", "Motijheel", "Mugda", "New Market", "Pallabi", "Paltan", 
-  "Ramna", "Rampura", "Sabujbagh", "Shah Ali", "Shahbag", "Shahjahanpur", 
-  "Sher-e-Bangla Nagar", "Shyampur", "Sutrapur", "Tejgaon", "Tejgaon Industrial Area", 
-  "Turag", "Uttara East", "Uttara West", "Uttara Khan", "Vatara", "Wari"
-];
+// Mapping of District ID to Metropolitan City Thanas
+const metroMap = {
+  // Dhaka
+  "1": [
+    "Adabor", "Airport", "Badda", "Banani", "Bangshal", "Bhashantek", "Cantonment", 
+    "Chawkbazar", "Dakshinkhan", "Dar-us-Salam", "Demra", "Dhanmondi", "Gendaria", 
+    "Gulshan", "Hatirjheel", "Hazaribagh", "Jatrabari", "Kadamtali", "Kafrul", 
+    "Kalabagan", "Kamrangirchar", "Khilgaon", "Khilkhet", "Kotwali", "Lalbagh", 
+    "Mirpur", "Mohammadpur", "Motijheel", "Mugda", "New Market", "Pallabi", "Paltan", 
+    "Ramna", "Rampura", "Sabujbagh", "Shah Ali", "Shahbag", "Shahjahanpur", 
+    "Sher-e-Bangla Nagar", "Shyampur", "Sutrapur", "Tejgaon", "Tejgaon Industrial Area", 
+    "Turag", "Uttara East", "Uttara West", "Uttara Khan", "Vatara", "Wari"
+  ],
+  // Gazipur
+  "3": [
+    "Bason", "Gacha", "Kashimpur", "Konabari", "Pubail", "Sadar", "Tongi East", "Tongi West"
+  ],
+  // Rajshahi
+  "24": [
+    "Airport", "Belpukur", "Boalia", "Chandrima", "Damkura", "Karnahar", "Kashiadanga", 
+    "Katakhali", "Matihar", "Paba", "Rajpara", "Shah Makhdum"
+  ],
+  // Rangpur
+  "32": [
+    "Haragach", "Hazirhat", "Kotwali", "Mahiganj", "Parshuram", "Tajhat"
+  ],
+  // Barishal
+  "35": [
+    "Airport", "Bandar", "Kaunia", "Kotwali"
+  ],
+  // Chattogram
+  "43": [
+    "Akbarshah", "Bakalia", "Bandar", "Bayezid Bostami", "Chandgaon", "Chawkbazar", 
+    "Double Mooring", "EPZ", "Halishahar", "Karnafuli", "Khulshi", "Kotowali", 
+    "Pahartali", "Panchlaish", "Patenga", "Sadarghat"
+  ],
+  // Cumilla
+  "44": [
+    "Kotwali", "Sadar Dakshin"
+  ],
+  // Sylhet
+  "54": [
+    "Airport", "Dakshin Surma", "Jalalabad", "Kotwali", "Mogla Bazar", "Shah Paran"
+  ],
+  // Khulna
+  "59": [
+    "Arongghata", "Daulatpur", "Harintana", "Khalishpur", "Khan Jahan Ali", 
+    "Khulna Sadar", "Labanchara", "Sonadanga"
+  ]
+};
 
 export function getDistricts() {
   return districts;
@@ -22,11 +62,11 @@ export function getDistricts() {
 export function getUpazilasByDistrict(districtId) {
   let results = upazilas.filter((u) => u.district_id === districtId);
   
-  // If Dhaka (ID: 1) is selected, inject Metropolitan Thanas
-  if (districtId === "1") {
-    const metroObjects = dhakaMetroThanas.map((name, index) => ({
-      id: `metro-${index}`,
-      district_id: "1",
+  // Inject Metropolitan Thanas if the district has any
+  if (metroMap[districtId]) {
+    const metroObjects = metroMap[districtId].map((name, index) => ({
+      id: `metro-${districtId}-${index}`,
+      district_id: districtId,
       name: name,
       bn_name: name // Kept English for now as fallback
     }));
@@ -43,4 +83,3 @@ export default {
   getDistricts,
   getUpazilasByDistrict
 };
-
